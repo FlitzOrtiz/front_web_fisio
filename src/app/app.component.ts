ff6be26fa37078e7d6c5fc0b0210c68830c9235c
HACHEROMAN <josumendez2011@hotmail.com>
2025-05-03 03:09:10 -0500

    Nuevo con login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/pages/landing/landing.component';
+import { LoginComponent } from './auth/component/login/login.component';
+import { RegisterComponent } from './auth/component/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingComponent],
+  imports: [RouterOutlet, LandingComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front_web_fisio';
 }
+
