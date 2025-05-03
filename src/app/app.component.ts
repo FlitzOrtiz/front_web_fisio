135b292a9f7561702938a871633c4608347abe4a
FlitzOrtiz <aaron.ortiz@ucuenca.edu.ec>
2025-05-03 00:47:30 -0500

    cambios en botn primera parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FbuttonComponent } from './common/component/fbutton/fbutton.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FbuttonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'front_web_fisio';
+
+  handleClick() {
+    alert('Button clicked!');
+  }
 }
---
+++ src/app/common/component/fbutton/fbutton.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fbutton',
+  imports: [CommonModule],
+  templateUrl: './fbutton.component.html',
+  styleUrl: './fbutton.component.scss'
+})
+export class FbuttonComponent {
+
+  @Input() label: string = 'Button';
+  @Input() type: string = 'button'; // 'button', 'submit', 'reset'
+  @Input() color: string = 'primary'; // 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
+  @Input() size: string = 'md'; // 'sm', 'md', 'lg'
+  @Input() disabled: boolean = false; // true or false
+  @Input() icon: string = ''; // FontAwesome icon name, e.g. 'fa-solid fa-check'
+  @Input() iconPosition: string = 'left'; // 'left', 'right'
+  @Input() loading: boolean = false; // true or false
+  @Input() loadingText: string = 'Loading...'; // Text to show when loading
+  @Input() loadingIcon: string = 'fa-solid fa-spinner'; // FontAwesome icon name for loading state, e.g. 'fa-solid fa-spinner'
+  @Input() loadingIconPosition: string = 'left'; // 'left', 'right'
+  @Input() loadingSize: string = 'sm'; // 'sm', 'md', 'lg'
+  @Input() loadingColor: string = 'primary'; // 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
+  @Input() loadingDisabled: boolean = false; // true or false
+
+  @Input() onClick: () => void = () => {}; // Function to call on button click
+
+
+  constructor(private el: ElementRef) {
+    
+  }
+
+  ngOnInit() {
+    // Initialize any properties or perform any setup here
+  }
+    
+}
