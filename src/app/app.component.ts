afcaf882d5847cc95412041096f891852ebf90d1
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-05-03 01:57:56 -0500

    Changed started page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FbuttonComponent } from './common/component/fbutton/fbutton.component';
+import { LandingComponent } from './landing/pages/landing/landing.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FbuttonComponent],
+  imports: [RouterOutlet, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front_web_fisio';
-
-  handleClick() {
-    alert('Button clicked!');
-  }
 }
