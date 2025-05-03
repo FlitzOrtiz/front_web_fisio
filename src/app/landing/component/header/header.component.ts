b9b96cf44d1e383d853cfa3f967e8e9f2d65ea22
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-05-02 23:22:37 -0500

    Created header component

--- src/app/landing/component/header/header.component.ts
+++ src/app/landing/component/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
