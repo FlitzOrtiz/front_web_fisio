9cce5970038543b7631c29639d6beac9d7e353a3
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-05-03 01:40:46 -0500

    Added welcome section in landing pae

--- src/app/landing/pages/landing/landing.component.ts
+++ src/app/landing/pages/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../component/header/header.component';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { WelcomeComponent } from '../../component/welcome/welcome.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, WelcomeComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
