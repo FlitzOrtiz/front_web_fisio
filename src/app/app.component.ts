a52464c9d1d3900db9781e7b306afd6fa9291428
SPad7 <sebaspad774@gmail.com>
2025-05-06 08:38:23 -0500

    dashboard responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/pages/landing/landing.component';
 import { LoginComponent } from './auth/component/login/login.component';
 import { RegisterComponent } from './auth/component/register/register.component';
+import { DashboardComponent } from './physio/pages/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingComponent, LoginComponent, RegisterComponent],
+  imports: [
+    RouterOutlet,
+    LandingComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front_web_fisio';
 }
-
