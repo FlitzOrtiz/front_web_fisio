c0037841a34c7ad6b3c134e73d67f04565d83c89
HACHEROMAN <josumendez2011@hotmail.com>
2025-05-06 08:23:51 -0500

    nuevo-routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/pages/landing/landing.component';
 import { LoginCodeComponent } from './routine/component/login-code/login-code.component';
-import { LoginCameraComponent } from './routine/component/login-camera/login-camera.component';
+import { LoginComponent } from './auth/component/login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingComponent, LoginCodeComponent, LoginCameraComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>` 
+  //imports: [RouterOutlet, LoginComponent],
+  //templateUrl: './app.component.html',
+  //styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front_web_fisio';
