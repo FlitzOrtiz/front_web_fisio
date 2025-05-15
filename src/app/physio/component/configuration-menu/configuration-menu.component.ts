5213d6c151a4aff55ec08e956ab0c37c89a7cb3e
HACHEROMAN <josumendez2011@hotmail.com>
2025-05-14 19:30:01 -0500

    new page account

---
+++ src/app/physio/component/configuration-menu/configuration-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-menu',
+  templateUrl: './configuration-menu.component.html',
+  styleUrls: ['./configuration-menu.component.scss']
+})
+export class ConfigurationMenuComponent {
+  userName = 'Jessie Brown';
+  userNickname = 'Jessie';
+  profileImage = 'https://i.pinimg.com/736x/e9/ea/ba/e9eabad5f189d26b31f77a03570ffea7.jpg';
+}
+//ddw
