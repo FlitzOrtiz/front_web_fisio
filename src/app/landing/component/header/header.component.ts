e8009c07fc2c95d5b610a6a0a201be66210ab45f
Joshua Cueva <joshua.cueva@ucuenca.edu.ec>
2025-05-18 20:34:55 -0500

    Implemented responsiveness to landing page

--- src/app/landing/component/header/header.component.ts
+++ src/app/landing/component/header/header.component.ts
@@ -1,21 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FbuttonComponent } from '../../../common/component/fbutton/fbutton.component';
+import { MobileMenuComponent } from '../mobile-menu/mobile-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, FbuttonComponent],
+  imports: [CommonModule, FbuttonComponent, MobileMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isMobileMenuOpen: boolean = false;
 
   scrollTo(id: string) {
-  const element = document.getElementById(id);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   }
-}
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMobileMenu() {
+    if (this.isMobileMenuOpen) {
+      this.isMobileMenuOpen = false;
+      document.body.style.overflow = '';
+    }
+  }
+
+  handleMenuItemClick(id: string) {
+    this.scrollTo(id);
+    this.closeMobileMenu();
+  }
 }
