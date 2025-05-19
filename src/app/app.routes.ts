4de9c911011f4c6ceb488e7c0034f249038b65b0
HACHEROMAN <josumendez2011@hotmail.com>
2025-05-18 23:43:43 -0500

    Fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,7 @@ import { LoginComponent } from './auth/component/login/login.component';
 import { RegisterComponent } from './auth/component/register/register.component';
 import { LandingComponent } from './landing/pages/landing/landing.component';
 import { LoginCodeComponent } from './routine/component/login-code/login-code.component';
-import { LoginCameraComponent } from './routine/component/login-camera/login-camera.component';
 import { DashboardComponent } from './physio/pages/dashboard/dashboard.component';
-import { GameCounterComponent } from './routine/component/game-counter/game-counter.component';
 import { AccountComponent } from './physio/pages/account/account.component';
 import { PrivacyComponent } from './physio/pages/privacy/privacy.component';
 import { ChangePasswordComponent } from './physio/pages/change-password/change-password.component';
@@ -16,7 +14,7 @@ import { RoutineCreatorComponent } from './physio/pages/routine-manage/routine-c
 import { MedicalRecordComponent } from './physio/pages/medical-record/medical-record.component';
 import { PatientRecordComponent } from './physio/pages/medical-record/patient-record/patient-record.component';
 import { ExerciseComponent } from './routine/pages/exercise/exercise.component';
-import { ExerciseSummaryComponent } from './routine/pages/exercise-summary/exercise-summary.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
@@ -24,9 +22,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'login/code', component: LoginCodeComponent },
-  { path: 'login/camera', component: LoginCameraComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'game/counter', component: GameCounterComponent },
   { path: 'account', component: AccountComponent },
   { path: 'privacy', component: PrivacyComponent },
   { path: 'change/password', component: ChangePasswordComponent },
@@ -38,5 +34,4 @@ export const routes: Routes = [
   { path: 'medicalrecordmanage', component: MedicalRecordComponent },
   { path: 'patientrecord', component: PatientRecordComponent },
   { path: 'exercise', component: ExerciseComponent },
-  { path : 'exercise/summary', component: ExerciseSummaryComponent },
 ];
